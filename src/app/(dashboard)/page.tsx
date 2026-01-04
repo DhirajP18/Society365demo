4bb445995df82f19608123621dc63230858ac3ce
Nikhil - Asus Home <Niknandeshwar09@gmail.com>
2026-01-05 00:49:38 +0530

    Initial setup & folder structure

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function DashboardPage() {
+  return (
+    <div className="p-6">
+      <Button>Society365 Dashboard</Button>
+    </div>
+  )
+}
